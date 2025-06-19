15d54f7d8d6efa5e5e00b4867d87d96f2a1ec6e8
leah <leahpooran824@gmail.com>
2025-06-19 11:41:54 +0200

    comments for the third screen

--- app/src/main/java/com/example/practicummusicplaylistmanagerapp/MainActivity3.kt
+++ app/src/main/java/com/example/practicummusicplaylistmanagerapp/MainActivity3.kt
@@ -19,39 +19,48 @@ class MainActivity3 :  AppCompatActivity() {
         val displayBtn = findViewById<Button>(R.id.displayBtn)
         val averageBtn = findViewById<Button>(R.id.averageBtn)
 
+
         displayBtn.setOnClickListener {
             var result = ""
 
+            // Loop through all saved songs
             for (i in 0 until count) {
                 result = result + "Song: " + songTitles[i] + "\n"
                 result = result + "Artist: " + artistNames[i] + "\n"
-                result = result + "Rating: " + ratings[i] + "/5\n"
+                result = result + "Rating: " + ratings[i] + "out of 5\n"
                 result = result + "Comment: " + comments[i] + "\n"
                 result = result + "-------------------\n"
             }
 
+            // If no songs were saved, show a message
             if (result == "") {
                 displayTextview.text = "No songs saved."
             } else {
+                // Else will display the result in the TextView
                 displayTextview.text = result
             }
         }
 
         averageBtn.setOnClickListener {
             if (count == 0) {
+                //when average button is clicked,if no songs saved yet then will show this message
                 displayTextview.text = "No ratings yet."
             } else {
-                var total = 0
+                var total = 0  // A variable to store the sum of ratings
+
+                // Loop through ratings to get total
                 for (i in 0 until count) {
                     total = total + ratings[i]
                 }
+
+                // Calculate average rating
                 val average = total / count
                 displayTextview.text = "Average Rating: $average"
             }
         }
 
         returnBtn.setOnClickListener {
-            finish() // return to main screen
+            finish()
         }
     }
 }
